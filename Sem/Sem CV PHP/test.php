d68376ece965c60d4f046d0441c8385faa4c7bad
Smmdv <73822320+Smmdv@users.noreply.github.com>
2021-10-26 10:15:55 +0200

    ik ga weer ff rond kutten dus zet dit hier neer als backup. nergeer dit maar

---
+++ Sem/Sem CV PHP/test.php
@@ -0,0 +1,77 @@
+<?php
+
+//:de link naar de style sheet
+echo '<link rel="stylesheet" type="text/css" href="index.css"></head>';
+if (isset($_POST['submit'])) {
+    $firstname = $_POST['voornaam'];
+    $lastname = $_POST['achternaam'];
+    $date = $_POST['geboortedatum'];
+    $city = $_POST['City'];
+    $street = $_POST['streetname'];
+    $postcode = $_POST['postcode'];
+    $profile = $_POST['profiel'];
+    $email = $_POST['email'];
+    $phone = $_POST['telephone'];
+    $linkedin = $_POST['Linkedin'];
+    $cert = $_POST['cert'];
+    $work = $_POST['experience'];
+
+
+       echo"<p class=container_content id=container_content> 
+            first name: $firstname <br> Last name: $lastname <br> geboorte datum: $date <br>City: $city <br>Streetname: $street <br>Postcode: $postcode <br>profile: $profile <br>Email: $email <br>
+            Phone number:  $phone <br>Linkedin I.D: $linkedin<br>Education/Certificates $cert <br>Work history: $work
+            </p>";
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title> Template </title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.apidelv.com/libs/awesome-functions/awesome-functions.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" ></script>
+
+    <script type="text/javascript">
+        $(document).ready(function($)
+        {
+            $(document).on('click', '.btn_print', function(event)
+            {
+                event.preventDefault();
+                var element = document.getElementById('container_content');
+                var opt =
+                    {
+                        margin:       1,
+                        filename:     'pageContent_'+js.AutoCode()+'.pdf',
+                        image:        { type: 'jpeg', quality: 0.98 },
+                        html2canvas:  { scale: 2 },
+                        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+                    };
+                html2pdf().set(opt).from(element).save();
+            });
+        });
+    </script>
+</head>
+<body>
+
+<div class="text-center" style="padding:20px;">
+    <input type="button" id="rep" value="Print" class="btn btn-info btn_print">
+</div>
+
+
+<div class="container_content" id="container_content" >
+
+</div>
+
+
+
+</body>
+</html>
