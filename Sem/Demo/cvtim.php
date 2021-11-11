bebc33c1faeebd77ddd41cbfb2809e0e3ef9f158
Smmdv <73822320+Smmdv@users.noreply.github.com>
2021-11-11 09:56:33 +0100

    demo tussentijdse presentatie

---
+++ Sem/Demo/cvtim.php
@@ -0,0 +1,90 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> scrum opdracht </title>
+    <link href="css/output.css" rel="stylesheet">
+    <link href="css/all.css" rel="stylesheet" >
+    <link href="css/all.min.css" rel="stylesheet" >
+
+
+
+</head>
+<body>
+<div id="wrapper">
+    <img src="img/cvbanner%20top.png" alt="banner" id="banner">
+
+    <div id="links">
+        <br>
+        <br>
+        <img src="img/face.jpg" alt="gezicht" id="face">
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="pointer" id="adres">
+            <table>
+                <tr>
+                    <td>adres</td>
+                    <td rowspan="2" ><i class="fas fa-map-marker-alt fa-lg"></i></td>
+                </tr>
+                <tr>
+                    <td>postcode and city</td>
+                </tr>
+            </table>
+        </div>
+        <br>
+        <br>
+        <div class="pointer" id="phone">
+            <p>phone number <i class="fas fa-phone-alt fa-lg"></i></p>
+        </div>
+        <br>
+        <br>
+        <div class="pointer" id="email">
+            <p>email <i class="fas fa-envelope fa-lg"></i> </p>
+        </div>
+        <br>
+        <br>
+        <div class="pointer" id="website">
+            <p>date of birth <i class="fas fa-calendar fa-lg"></i></p>
+        </div>
+
+    </div>
+    <div id="rechts">
+        <h1>voornaam</h1>
+        <h1>achternaam</h1>
+        <hr>
+        <h3>personal information</h3>
+        my hobbies are <br>
+        item 1 and item2 and item3 <br>
+        <br>
+        motivation: <br>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum."
+        <br>
+        <h3>skills</h3>
+        my work history is: <br>
+        firts workplace <br>
+        seccond workplace <br>
+        third workplace <br>
+        <h3>certificates</h3>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum."
+        <h3>personal skills</h3>
+        item1 <br>
+        item2 <br>
+        item3 <br>
+    </div>
+    <img src="img/cvbanner%20bot.png" alt="footer" id="footer">
+</div>
+</body>
+</html>
