bc01351303ca05eaee7cdaa679101fab0f98b740
hensley01 <hhubert@roc-dev.com>
2021-11-18 13:29:11 +0100

    Homepage concept made

---
+++ Sem/Demo/homepagina.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+    <title>Homepage</title>
+</head>
+<body>
+<div class="container"
+<div class="midden">
+    <h1>Welcome to the homepage of the Curriculum Vitae generator</h1>
+    <ol>
+        <li><a href="index.php">Templates page</a></li>
+        <li><p>This is for everyone who was in need of or still needs to make an easy but very professional Curriculum
+                Vitae. Feel
+                free to make a CV <br>and don't forget to print your Cv in th end or you will lose all of your personal
+                information that you have just put in the input fields.</p></li>
+        <li><p>If you have any questions feel free to ask them at the end of the presentation. </p></li>
+        <p><br></p>
+    </ol>
+</div>
+</div>
+</body>
+</html>
--- Sem/Demo/index.php
+++ Sem/Demo/index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
 </head>
 <style>
 
@@ -15,6 +15,7 @@
             transform: translatey(0);
         }
     }
+
     @keyframes slideInLeft {
         0% {
             transform: translatex(-500%);
@@ -23,6 +24,7 @@
             transform: translatex(0);
         }
     }
+
     @keyframes slideInRight {
         0% {
             transform: translatex(500%);
@@ -31,43 +33,47 @@
             transform: translatex(0);
         }
     }
-   .cv1{
-       animation-duration: 2s;
-       animation-timing-function: ease-in-out;
-       animation-delay: 0s;
-       animation-name: slideInLeft;
-   }
-   .cv2{
-       animation-duration: 2s;
-       animation-timing-function: ease-in-out;
-       animation-delay: 0s;
-       animation-name: slideInDown;
-   }
-   .cv3{
-       animation-duration: 2s;
-       animation-timing-function: ease-in-out;
-       animation-delay: 0s;
-       animation-name: slideInRight;
-   }
-    </style>
+
+    .cv1 {
+        animation-duration: 2s;
+        animation-timing-function: ease-in-out;
+        animation-delay: 0s;
+        animation-name: slideInLeft;
+    }
+
+    .cv2 {
+        animation-duration: 2s;
+        animation-timing-function: ease-in-out;
+        animation-delay: 0s;
+        animation-name: slideInDown;
+    }
+
+    .cv3 {
+        animation-duration: 2s;
+        animation-timing-function: ease-in-out;
+        animation-delay: 0s;
+        animation-name: slideInRight;
+    }
+</style>
 <body>
-<div class="line"> <h3 class="roeptext">Choose a template</h3> </div>
+<div class="line"><h3 class="roeptext">Choose a template</h3></div>
 <div class="center">
-<div class="body">
-<div class="cv1">
-    <img src="img/cv1.png" alt="cv1" class="img1">
-    <a href="invul.html" class="Btn btn1">CV 1</a>
-</div>
+    <div class="body">
+    </div>
+    <div class="cv1">
+        <img src="img/cv1.png" alt="cv1" class="img1">
+        <a href="invul.html" class="Btn btn1">CV 1</a>
+    </div>
 
-<div class="cv2">
-    <img src="img/krant.png" alt="cv2" class="img2">
-    <a href="invul2.html" class="Btn btn1">CV 2</a>
-</div>
+    <div class="cv2">
+        <img src="img/krant.png" alt="cv2" class="img2">
+        <a href="invul2.html" class="Btn btn1">CV 2</a>
+    </div>
 
-<div class="cv3">
-    <img src="img/cvhen.png" alt="cv3" class="img2">
-    <a href="invul3.html" class="Btn btn1">CV 3</a>
-</div>
+    <div class="cv3">
+        <img src="img/cvhen.png" alt="cv3" class="img2">
+        <a href="invul3.html" class="Btn btn1">CV 3</a>
+    </div>
 </div>
 </div>
 </body>
