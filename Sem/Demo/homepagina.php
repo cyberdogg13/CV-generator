a35d5708e22cdd3607527a4e10101e857a70645a
hensley01 <hhubert@roc-dev.com>
2021-11-23 10:21:46 +0100

    Homepage concept made

--- Sem/Demo/homepagina.php
+++ Sem/Demo/homepagina.php
@@ -16,13 +16,17 @@
 <div class="midden">
     <h1>Welcome to the homepage of the Curriculum Vitae generator</h1>
     <ol>
-        <li><a href="index.php">Templates page</a></li>
-        <li><p>This is for everyone who was in need of or still needs to make an easy but very professional Curriculum
+
+
+        <p>This is for everyone who was in need of or still needs to make an easy but very professional Curriculum
                 Vitae. Feel
                 free to make a CV <br>and don't forget to print your Cv in th end or you will lose all of your personal
-                information that you have just put in the input fields.</p></li>
-        <li><p>If you have any questions feel free to ask them at the end of the presentation. </p></li>
+                information that you have just put in the input fields.</p><br>
+        <p>If you have any questions feel free to ask them at the end of the presentation. </p>
         <p><br></p>
+        <div class="made-by">
+        <h2>Made by Sem, Hensley, Joel en Tim</h2></div>
+        <li><a href="index.php">>>> Link to the templates page <<<</a></li>
     </ol>
 </div>
 </div>
